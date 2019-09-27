4db8a390bf7b382776b9a9125e424f0d9df7b7a7
Forrest <11416821+xfox64x@users.noreply.github.com>
2019-09-27 15:06:39 -0400

    Update build_toolchain.sh

--- build_toolchain.sh
+++ build_toolchain.sh
@@ -1,4 +1,5 @@
-#!/bin/bash 
+#!/bin/bash
+# Original script stolen from: https://www.held-im-ruhestand.de/software/embedded/cross-compiler.html 
 set -e
 set -u
 
@@ -6,9 +7,8 @@ set -u
 BASE=$(dirname "$0")
 BASE=$(cd "$BASE" && pwd)
 
-# target machine tuple. This is suitable for the marvell sheevaplug
 TARGET=arm-unknown-linux-gnueabi
-BUILD=$(gcc -dumpmachine)        # x86_64-unknown-linux-gnu, i686-linux-gnu, ...
+BUILD=$(gcc -dumpmachine)
 ARCH=arm
 
 SYSROOT="$BASE/sysroot"     # files for the target system 
@@ -16,7 +16,7 @@ TOOLCHAIN="$BASE/toolchain" # the cross compilers
 SRCDIR="$BASE/src"          # saving tarballs
 WORKDIR="$BASE/work"        # unpacked tarballs
 BUILDDIR="$BASE/build"      # running compile 
-LOGDIR="$BASE/logs"
+LOGDIR="$BASE/logs"         # various logs
 
 PARALLEL="-j2"              # dualcore
 
@@ -27,7 +27,8 @@ GCC=gcc-4.5.1
 GMP=gmp-4.3.1
 MPC=mpc-0.8.1
 MPFR=mpfr-2.4.2
-EGLIBC=eglibc-2_11          # abi compatible to glibc, better support for arm
+GLIBC=glibc-2.11.1
+GLIBCPORTS=glibc-ports-2.11
 
 GREEN="\e[38;5;118m"
 RED="\e[38;5;203m"
@@ -39,6 +40,31 @@ GOOD="[$GREEN+$END]"
 BAD="[$RED-$END]"
 STAT="[$GREY*$END]"
 WARN="[$YELLOW!$END]"
+
+
+success_message(){
+    echo -e "$GREEN"
+    echo " ___                    "
+    echo "|   \  ___  _ __   ___  "
+    echo "| |\ |/ _ \| '_ \ / _ \ "
+    echo "| |/ | (_) | | | |  __/ "
+    echo "|___/ \___/|_| |_|\___| "
+    echo "                        "
+    echo -e "$END"
+    exit 1
+}
+
+failed_message(){
+    echo -e "$RED"
+    echo " _____       _  _           _ "
+    echo "| ____| ___ |_|| | ___   __| |"
+    echo "| |_   / _ \ _ | |/ _ \ / _  |"
+    echo "| __| ( (_) | || |  __/( (_| |"
+    echo "|_|    \__|_|_||_|\___| \__,_|"
+    echo "                              "
+    echo -e "$END"
+    exit 1
+}
 
 install_missing_package() {
     PACKAGE_NAME=$1
@@ -59,9 +85,23 @@ install_missing_package() {
     fi
 }
 
+make_dir() {
+    dir_path=$1
+    if [ ! -d "$dir_path" ]; then
+        echo "mkdir -p $dir_path" >> $COMMAND_LOG
+        mkdir -p $dir_path
+    fi
+}
+
+do_patch(){
+    target=$1
+    patch_path=$2
+    echo "patch $target $patch_path" >> $COMMAND_LOG
+    patch $target $patch_path
+}
+
 # download functions
 get_url() {
-	# pass a full url as parameter
 	url=$1
 	file=${url##*/}
 	if [ ! -f "$SRCDIR/$file" ]; then
@@ -74,22 +114,34 @@ get_url() {
 unpack() {
 	# pass a filename as parameter
 	file=$1
+	destination=${2:-$WORKDIR}
+	new_name=${3:-""}
 	ext=${file##*.}
 	folder=${file%%.tar.*}
-	if [ !  -d "$WORKDIR/$folder"  ]; then
-		echo -e "$STAT unpacking $file..."
-	else
+
+    if [ "$new_name" != "" ]; then
+        if [ -d "$destination/$new_name" ]; then
+    		return 0
+    	fi
+    fi
+
+    # if untar'd dir already exists, don't untar again.
+	if [ -d "$destination/$folder" ]; then
 		return 0
-	fi
-	echo "cd $WORKDIR" >> $COMMAND_LOG
-	cd $WORKDIR
-	if [ $ext == "bz2" ]; then
-		echo "tar jxf \"$SRCDIR/$file\"" >> $COMMAND_LOG
-		tar jxf "$SRCDIR/$file"
 	else
-		echo "tar zxf \"$SRCDIR/$file\"" >> $COMMAND_LOG
-		tar zxf "$SRCDIR/$file"
+		echo -e "$STAT unpacking $file to $WORKDIR..."
 	fi
+	tar_args="zxf"
+	if [ "$ext" == "bz2" ]; then
+	    tar_args="jxf"
+    fi
+    echo "tar $tar_args $SRCDIR/$file -C $destination" >> $COMMAND_LOG
+    tar $tar_args $SRCDIR/$file -C $destination
+    
+    if [ "$new_name" != "" ]; then
+        echo "mv -v $destination/$folder $destination/$new_name" >> $COMMAND_LOG
+        mv -v $destination/$folder $destination/$new_name
+    fi
 }
 
 check_done() {
@@ -139,7 +191,7 @@ binutils() {
 			--disable-nls \
 			--disable-werror
 	fi
-	do_msg $OBJ "do make"
+	do_msg $OBJ "do     make"
 	echo "make $PARALLEL 2>&1 | tee $LOGDIR/$OBJ""_make.log" >> $COMMAND_LOG
 	make $PARALLEL 2>&1 | tee "$LOGDIR/$OBJ""_make.log"
 	do_msg $OBJ "do make install to $TOOLCHAIN/$TARGET"
@@ -188,10 +240,21 @@ gccstatic() {
 	do_msg $OBJ "do make install to $TOOLCHAIN/$TARGET"
 	echo "PATH=$TOOLCHAIN/bin:$PATH make install 2>&1 | tee $LOGDIR/$OBJ""_make_install.log" >> $COMMAND_LOG
 	PATH=$TOOLCHAIN/bin:$PATH \
-	make install 2>&1 | tee "$LOGDIR/$OBJ""_make_install.log" 
-	do_msg $OBJ "done"
-	touch $BUILDDIR/$OBJ.done
-	echo "touch $BUILDDIR/$OBJ.done" >> $COMMAND_LOG
+	make install 2>&1 | tee "$LOGDIR/$OBJ""_make_install.log"
+
+    if [ -f $TOOLCHAIN/lib/gcc/$TARGET/$(echo $GCC | cut -d "-" -f2)/libgcc.a ]; then
+        ln -vs $TOOLCHAIN/lib/gcc/$TARGET/$(echo $GCC | cut -d "-" -f2)/libgcc.a `/$TOOLCHAIN/bin/$TARGET-gcc -print-libgcc-file-name | sed 's/libgcc/&_eh/'`
+        do_msg $OBJ "Successfully compiled"
+    	echo "touch $BUILDDIR/$OBJ.done" >> $COMMAND_LOG
+    	touch $BUILDDIR/$OBJ.done
+    else
+        do_msg $OBJ "Failed to compile" "BAD"
+        echo -e "$BAD$RED Grep'd errors from log files:$END\n"
+        grep "Error" "$LOGDIR/$OBJ""_make.log"
+        grep "Error" "$LOGDIR/$OBJ""_make_install.log"
+        echo "\n"
+        failed_message
+    fi
 	popd
 }
 
@@ -215,10 +278,10 @@ gccminimal() {
 	fi
 	do_msg $OBJ "compile"
 	PATH=$TOOLCHAIN/bin:$PATH \
-	make $PARALLEL 2>&1 | tee $BUILDDIR/$OBJ/make.log
+	make $PARALLEL 2>&1 | tee "$LOGDIR/$OBJ""_make.log"
 	do_msg $OBJ "install"
 	PATH=$TOOLCHAIN/bin:$PATH \
-	make install 2>&1 | tee $BUILDDIR/$OBJ/make_install.log 
+	make install 2>&1 | tee "$LOGDIR/$OBJ""_make_install.log" 
 	do_msg $OBJ "done"
 	touch $BUILDDIR/$OBJ.done
 	popd
@@ -241,42 +304,41 @@ gccfull() {
 			--disable-libmudflap \
 			--enable-languages=c,c++ \
 			--disable-nls 
-	# now with c++
-	# the cxy_atexit is special for eglibc
 	fi
 	do_msg $OBJ "compile"
 	PATH=$TOOLCHAIN/bin:$PATH \
-	make $PARALLEL 2>&1 | tee $BUILDDIR/$OBJ/make.log
+	make $PARALLEL 2>&1 | tee "$LOGDIR/$OBJ""_make.log"
 	do_msg $OBJ "install"
 	PATH=$TOOLCHAIN/bin:$PATH \
-	make install 2>&1 | tee $BUILDDIR/$OBJ/make_install.log 
+	make install 2>&1 | tee "$LOGDIR/$OBJ""_make_install.log" 
 	do_msg $OBJ "done"
 	touch $BUILDDIR/$OBJ.done
 	popd
 }
 
-kernelheader() {
-	# compiling headers that work on the target system
-	# this is done in-tree 
+kernelheader() { 
 	OBJ=$KERNEL
 	check_done $OBJ && return 0
 	pushd $WORKDIR/$OBJ
 	do_msg $OBJ "Starting Linux header make"
-	make mrproper
+	echo "make mrproper" >> $COMMAND_LOG
+	make mrproper 2>&1 | tee "$LOGDIR/$OBJ""_make.log"
 	do_msg $OBJ "Installing toolchain headers"
+	echo "PATH=$TOOLCHAIN/bin:$PATH make ARCH=$ARCH INSTALL_HDR_PATH=$SYSROOT/usr CROSS_COMPILE=$TARGET-headers_install" >> $COMMAND_LOG
 	PATH=$TOOLCHAIN/bin:$PATH \
 	make \
 		ARCH=$ARCH \
 		INSTALL_HDR_PATH=$SYSROOT/usr \
-		CROSS_COMPILE=$TARGET- \
-		headers_install 
+		CROSS_COMPILE=$TARGET-headers_install 2>&1 | tee "$LOGDIR/$OBJ""_make_install-headers.log"
+	cp -r $SYSROOT/usr/* $TOOLCHAIN/
 	do_msg $OBJ "done"
+	echo "touch $BUILDDIR/$OBJ.done" >> $COMMAND_LOG
 	touch $BUILDDIR/$OBJ.done
 	popd
 }
 
-eglibcheader() {
-	OBJ=$EGLIBC-header
+glibcheader() {
+	OBJ=$GLIBC-header
 	do_msg $OBJ "start"
 	check_done $OBJ && return 0
 	mkdir -p $BUILDDIR/$OBJ
@@ -288,7 +350,7 @@ eglibcheader() {
 		AR=$TOOLCHAIN/bin/$TARGET-ar \
 		LD=$TOOLCHAIN/bin/$TARGET-ld \
 		RANLIB=$TOOLCHAIN/bin/$TARGET-ranlib \
-		$WORKDIR/$EGLIBC/configure \
+		$WORKDIR/$GLIBC/configure \
 			--prefix=/usr \
 			--with-headers=$SYSROOT/usr/include \
 			--build=$BUILD \
@@ -297,15 +359,18 @@ eglibcheader() {
 			--disable-profile \
 			--without-gd \
 			--without-cvs \
-			--enable-add-ons
+			#--enable-shared \
+			--enable-add-ons=nptl,ports \
+			libc_cv_forced_unwind=yes \
+            libc_cv_c_cleanup=yes
 	fi
 	do_msg $OBJ "install"
 	make \
 		install-headers \
 		install_root=$SYSROOT \
-		install-bootstrap-headers=yes
+		install-bootstrap-headers=yes 2>&1 | tee "$LOGDIR/$OBJ""_make_install-headers.log"
 	do_msg $OBJ "crtX and fake libc"
-	make csu/subdir_lib
+	make csu/subdir_lib 2>&1 | tee "$LOGDIR/$OBJ""_make_csu_lib.log"
 	mkdir -p $SYSROOT/usr/lib
 	cp csu/crt1.o csu/crti.o csu/crtn.o $SYSROOT/usr/lib
 	# build a dummy libc
@@ -314,45 +379,49 @@ eglibcheader() {
 		-nostartfiles \
 		-shared \
 		-x c /dev/null \
-		-o $SYSROOT/usr/lib/libc.so
-	do_msg $OBJ "done"
-	touch $BUILDDIR/$OBJ.done
-	popd
-}
-eglibc() {
-	OBJ=$EGLIBC
-	do_msg $OBJ "start"
-	check_done $OBJ && return 0
-	mkdir -p $BUILDDIR/$OBJ
-	pushd  $BUILDDIR/$OBJ
-	if [ ! -f Makefile ]; then
-		BUILD_CC=gcc \
-		CC=$TOOLCHAIN/bin/$TARGET-gcc \
-		CXX=$TOOLCHAIN/bin/$TARGET-g++ \
-		AR=$TOOLCHAIN/bin/$TARGET-ar \
-		RANLIB=$TOOLCHAIN/bin/$TARGET-ranlib \
-		$WORKDIR/$EGLIBC/configure \
-			--prefix=/usr \
-			--with-headers=$SYSROOT/usr/include \
-			--build=$BUILD \
-			--host=$TARGET \
-			--disable-nls \
-			--disable-profile \
-			--without-gd \
-			--without-cvs \
-			--enable-add-ons
-	fi
-	do_msg $OBJ "compile"
-	PATH=$TOOLCHAIN/bin:$PATH \
-	make $PARALLEL
-	do_msg $OBJ "install"
-	PATH=$TOOLCHAIN/bin:$PATH \
-	make install install_root=$SYSROOT
+		-o $SYSROOT/usr/lib/libc.so 2>&1 | tee "$LOGDIR/$OBJ""_dummy_libc.log"
 	do_msg $OBJ "done"
 	touch $BUILDDIR/$OBJ.done
 	popd
 }
 
+glibc() {
+	OBJ=$GLIBC
+	do_msg $OBJ "start"
+	check_done $OBJ && return 0
+	mkdir -p $BUILDDIR/$OBJ
+	pushd  $BUILDDIR/$OBJ
+	if [ ! -f Makefile ]; then
+	    BUILD_CC=gcc \
+	    CC=$TOOLCHAIN/bin/$TARGET-gcc \
+	    CXX=$TOOLCHAIN/bin/$TARGET-g++ \
+        AR=$TOOLCHAIN/bin/$TARGET-ar \
+        RANLIB=$TOOLCHAIN/bin/$TARGET-ranlib \
+		$WORKDIR/$GLIBC/configure \
+		--prefix=/usr \
+		--with-headers=$SYSROOT/usr/include \
+        --build=$BUILD \
+        --host=$TARGET \
+        --disable-nls \
+        --disable-profile \
+        --without-gd \
+        --without-cvs \
+        --enable-add-ons=nptl,ports\
+        #--enable-shared \
+        #--enable-kernel=$(echo $KERNEL | cut -d "-" -f2) \
+        libc_cv_forced_unwind=yes \
+        libc_cv_c_cleanup=yes
+	fi
+	do_msg $OBJ "compile"
+	PATH=$TOOLCHAIN/bin:$PATH \
+	make $PARALLEL 2>&1 | tee "$LOGDIR/$OBJ""_make.log"
+	do_msg $OBJ "install"
+	PATH=$TOOLCHAIN/bin:$PATH \
+	make install install_root=$SYSROOT 2>&1 | tee "$LOGDIR/$OBJ""_make_install.log"
+	do_msg $OBJ "done"
+	touch $BUILDDIR/$OBJ.done
+	popd
+}
 
 
 # ==============================================
@@ -362,112 +431,87 @@ mkdir -p $LOGDIR
 COMMAND_LOG="$LOGDIR/command.log"
 echo "" > $COMMAND_LOG
 
-install_missing_package "build-essential"
-install_missing_package "texinfo"
+# Install missing packages. (Not yet sure if we need libelf-dev)
+#install_missing_package "build-essential"
+#install_missing_package "texinfo"
+#install_missing_package "autoconf"
 #install_missing_package "libelf-dev"
 
+# Remove existing, contaminated directories.
 echo -e "$STAT Removing any existing build or work dirs..."
 echo "rm -rf $BUILDDIR" >> $COMMAND_LOG
 rm -rf $BUILDDIR
-echo "rm -rf $WORKDIR" >> $COMMAND_LOG
-rm -rf $WORKDIR
-#echo "rm -rf $LOGDIR" >> $COMMAND_LOG
-#rm -rf $LOGDIR
-#rm -rf $SYSROOT
-#rm -rf $TOOLCHAIN
-#rm -rf $SRCDIR
+#echo "rm -rf $WORKDIR" >> $COMMAND_LOG
+#rm -rf $WORKDIR
+echo "rm -f $LOGDIR/*" >> $COMMAND_LOG
+rm -f $LOGDIR/*
 
-echo -e "$STAT Creating directories..."
-echo "mkdir -p $BUILDDIR" >> $COMMAND_LOG
-mkdir -p $BUILDDIR
-echo "mkdir -p $WORKDIR" >> $COMMAND_LOG
-mkdir -p $WORKDIR
-echo "mkdir -p $SRCDIR" >> $COMMAND_LOG
-mkdir -p $SRCDIR
-echo "mkdir -p $SYSROOT" >> $COMMAND_LOG
-mkdir -p $SYSROOT
-echo "mkdir -p $TOOLCHAIN" >> $COMMAND_LOG
-mkdir -p $TOOLCHAIN
+# Create necessary directories to work with.
+make_dir $SYSROOT
+make_dir $TOOLCHAIN
+make_dir $SRCDIR
+make_dir $WORKDIR
+make_dir $BUILDDIR
 
+# Download fresh source files from the internet.
 echo -e "$STAT Downloading source files..."
 pushd $SRCDIR
-#get_url "https://ftp.gnu.org/gnu/binutils/$BINUTILS.tar.bz2"
-#get_url "https://ftp.gnu.org/gnu/gcc/$GCC/$GCC.tar.bz2"
-get_url "https://osmocom.org/attachments/download/2798/patch-gcc46-texi.diff"
-#get_url "https://mirrors.edge.kernel.org/pub/linux/kernel/v2.6/$KERNEL.tar.bz2"
-#get_url "https://ftp.gnu.org/gnu/gmp/$GMP.tar.gz"	
-#get_url "https://ftp.gnu.org/gnu/mpfr/$MPFR.tar.gz"
-#get_url "https://gcc.gnu.org/pub/gcc/infrastructure/$MPC.tar.gz"
+get_url "https://ftp.gnu.org/gnu/binutils/$BINUTILS.tar.bz2"
+get_url "https://ftp.gnu.org/gnu/gcc/$GCC/$GCC.tar.bz2"
+#get_url "https://osmocom.org/attachments/download/2798/patch-gcc46-texi.diff"
+get_url "https://mirrors.edge.kernel.org/pub/linux/kernel/v2.6/$KERNEL.tar.bz2"
+get_url "https://ftp.gnu.org/gnu/gmp/$GMP.tar.gz"	
+get_url "https://ftp.gnu.org/gnu/mpfr/$MPFR.tar.gz"
+get_url "https://gcc.gnu.org/pub/gcc/infrastructure/$MPC.tar.gz"
+get_url "https://ftp.gnu.org/gnu/libc//$GLIBC.tar.gz"
+get_url "https://ftp.gnu.org/gnu/libc/$GLIBCPORTS.tar.gz"
+#get_url "www.linuxfromscratch.org/patches/downloads/glibc/glibc-2.11.1-gcc_fix-1.patch"
 
-echo -e "$STAT Unpacking source files into working directories..."
+# Unpack binutils and gcc
 unpack "$BINUTILS.tar.bz2"
 unpack "$GCC.tar.bz2"
-
-do_msg $GCC "Applying GCC docs gcc.texi patch..."
-echo "patch $WORKDIR/$GCC/gcc/doc/gcc.texi $SRCDIR/patch-gcc46-texi.diff" >> $COMMAND_LOG
-#cp $WORKDIR/$GCC/gcc/doc/gcc.texi $WORKDIR/$GCC/gcc/doc/gcc.texi.back
-patch $WORKDIR/$GCC/gcc/doc/gcc.texi $SRCDIR/patch-gcc46-texi.diff
-echo "patch $WORKDIR/$GCC/gcc/doc/cppopts.texi $SRCDIR/patch-cppopts.texi.diff" >> $COMMAND_LOG
-patch $WORKDIR/$GCC/gcc/doc/cppopts.texi $SRCDIR/patch-cppopts.texi.diff
-echo "patch $WORKDIR/$GCC/gcc/doc/invoke.texi $SRCDIR/patch-invoke-texi.diff" >> $COMMAND_LOG
-patch $WORKDIR/$GCC/gcc/doc/invoke.texi $SRCDIR/patch-invoke-texi.diff
-echo "patch $WORKDIR/$GCC/gcc/doc/generic.texi $SRCDIR/patch-generic-texi.diff" >> $COMMAND_LOG
-patch $WORKDIR/$GCC/gcc/doc/generic.texi $SRCDIR/patch-generic-texi.diff
-
-
-echo -e "$STAT Unpacking and renaming $GCC requirements..."
-echo "cp $SRCDIR/$GMP.tar.gz $WORKDIR/$GCC" >> $COMMAND_LOG
-cp $SRCDIR/$GMP.tar.gz $WORKDIR/$GCC
-echo "cp $SRCDIR/$MPFR.tar.gz $WORKDIR/$GCC" >> $COMMAND_LOG
-cp $SRCDIR/$MPFR.tar.gz $WORKDIR/$GCC
-echo "cp $SRCDIR/$MPC.tar.gz $WORKDIR/$GCC" >> $COMMAND_LOG
-cp $SRCDIR/$MPC.tar.gz $WORKDIR/$GCC
-
-echo "cd $WORKDIR/$GCC" >> $COMMAND_LOG
-cd $WORKDIR/$GCC
-
-echo "tar zxf $GMP.tar.gz" >> $COMMAND_LOG
-tar zxf $GMP.tar.gz
-echo "tar zxf $MPFR.tar.gz" >> $COMMAND_LOG
-tar zxf $MPFR.tar.gz
-echo "tar zxf $MPC.tar.gz" >> $COMMAND_LOG
-tar zxf $MPC.tar.gz
-echo "mv -v $GMP gmp" >> $COMMAND_LOG
-mv -v $GMP gmp
-echo "mv -v $MPFR mpfr" >> $COMMAND_LOG
-mv -v $MPFR mpfr
-echo "mv -v $MPC mpc" >> $COMMAND_LOG
-mv -v $MPC mpc
-echo "cd $BASE" >> $COMMAND_LOG
-cd $BASE
-
-echo -e "$GOOD Finished unpacking and renaming $GCC requirements."
-echo -e "$STAT Unpacking Linux headers for $KERNEL..."
 unpack "$KERNEL.tar.bz2"
+unpack "$GLIBC.tar.gz"
+unpack "$GMP.tar.gz" "$WORKDIR/$GCC" "gmp"
+unpack "$MPFR.tar.gz" "$WORKDIR/$GCC" "mpfr"
+unpack "$MPC.tar.gz" "$WORKDIR/$GCC" "mpc"
+unpack "$GLIBCPORTS.tar.gz" "$WORKDIR/$GLIBC" "ports"
 
-#if [ ! -h $WORKDIR/$EGLIBC ]; then
-#	ln -s $SRCDIR/$EGLIBC $WORKDIR/$EGLIBC
-#fi
+# Patching texinfo issues affecting gcc: https://osmocom.org/issues/1916
+# Not really sure how the syntax gets fucked up in only a few places...
+do_msg $GCC "Applying GCC docs gcc.texi patch..."
+do_patch $WORKDIR/$GCC/gcc/doc/gcc.texi $SRCDIR/patch-gcc46-texi.diff
+do_patch $WORKDIR/$GCC/gcc/doc/cppopts.texi $SRCDIR/patch-cppopts.texi.diff
+do_patch $WORKDIR/$GCC/gcc/doc/invoke.texi $SRCDIR/patch-invoke-texi.diff
+do_patch $WORKDIR/$GCC/gcc/doc/generic.texi $SRCDIR/patch-generic-texi.diff
 
-#echo "[*] Compiling $BINUTILS..."
-#binutils
+# Apply some patches because we're dirty futurers.
+# Some patches modified and taken from: http://www.linuxfromscratch.org/patches/downloads/glibc/
+do_patch $WORKDIR/$GLIBC/manual/Makefile $SRCDIR/patch-glibc-Makefile.diff
+do_patch $WORKDIR/$GLIBC/nptl/sysdeps/pthread/pt-initfini.c $SRCDIR/patch-glibc-pt-initfini-c.diff
+do_patch $WORKDIR/$GLIBC/sysdeps/unix/sysv/linux/i386/sysdep.h $SRCDIR/patch-glibc-sysdep-h.diff
+# Patching configure because we're from the future.
+do_patch $WORKDIR/$GLIBC/configure $SRCDIR/patch-glibc-configure.diff
+
+# Compile binutils.
+echo "[*] Compiling $BINUTILS..."
+binutils
+
+# Compile the static gcc, and create libgcc_eh.a as a link to libgcc.a
+# Because http://www.linuxfromscratch.org/lfs/view/6.7/chapter05/gcc-pass1.html told us to.
 echo -e "$STAT Compiling $GCC static binaries..."
 gccstatic
 
-#ln -vs libgcc.a `$TARGET-gcc -print-libgcc-file-name | sed 's/libgcc/&_eh/'`
+# Compile the kernel headers.
+echo -e "$STAT Compiling $KERNEL kernel headers..."
+kernelheader
 
-#kernelheaderll
-#eglibcheader
+# Compile the glibc headers (I have no idea what I'm doing).
+glibcheader
+
+gccminimal
+#glibc
 #gccminimal
-#eglibc
 #gccfull
 
-echo -e "$GREEN"
-echo "     _                   "
-echo "  __| | ___  _ __   ___  "
-echo " / _  |/ _ \| '_ \ / _ \ "
-echo "| (_| | (_) | | | |  __/ "
-echo " \__,_|\___/|_| |_|\___| "
-echo "                         "
-echo -e "$END"
-    
+success_message
